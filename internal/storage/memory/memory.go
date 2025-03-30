2221b30607a75f9bf202413e79afdd786b89ef8a
Denis Morozov <denmor86@gmail.com>
2025-03-30 16:09:26 +0300

    MemStorage. Renaming var

--- internal/storage/memory/memory.go
+++ internal/storage/memory/memory.go
@@ -6,28 +6,28 @@ import (
 )
 
 type MemStorage struct {
-	urlsMap map[string]string
+	Urls map[string]string
 	sync.RWMutex
 }
 
 func NewMemStorage() *MemStorage {
 	var s MemStorage
-	s.urlsMap = make(map[string]string)
+	s.Urls = make(map[string]string)
 	return &s
 }
 
-func (s *MemStorage) Save(urlBase string, urlShort string) {
+func (s *MemStorage) Save(longURL string, shortURL string) {
 	s.Lock()
-	s.urlsMap[urlShort] = urlBase
+	s.Urls[shortURL] = longURL
 	s.Unlock()
 }
 
-func (s *MemStorage) Load(urlShort string) (string, error) {
+func (s *MemStorage) Load(shortURL string) (string, error) {
 	s.Lock()
-	urlBase, exist := s.urlsMap[urlShort]
+	longURL, exist := s.Urls[shortURL]
 	s.Unlock()
 	if exist {
-		return urlBase, nil
+		return longURL, nil
 	}
-	return "", fmt.Errorf("short url not found: %s", urlShort)
+	return "", fmt.Errorf("short url not found: %s", shortURL)
 }
