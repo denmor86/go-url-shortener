7cb3ed5fce397a52f6d76e4d92ba85d15fa299f3
Morozov Denis <denmor86@gmail.com>
2025-04-15 23:10:57 +0300

    Add handler for /api/shorten with json body

--- internal/network/router/router.go
+++ internal/network/router/router.go
@@ -11,7 +11,8 @@ import (
 func HandleRouter(config config.Config, storage storage.IStorage) chi.Router {
 	r := chi.NewRouter()
 	r.Route("/", func(r chi.Router) {
-		r.Post("/", logger.RequestLogger(handlers.EncondeURLHandler(config.BaseURL, config.ShortURLLen, storage))) // POST /
+		r.Post("/", logger.RequestLogger(handlers.EncondeURLHandler(config.BaseURL, config.ShortURLLen, storage)))                // POST /
+		r.Post("/api/shorten", logger.RequestLogger(handlers.EncondeURLJsonHandler(config.BaseURL, config.ShortURLLen, storage))) // POST /api/shorten
 		r.Route("/{id}", func(r chi.Router) {
 			r.Get("/", logger.RequestLogger(handlers.DecodeURLHandler(storage))) // GET /shortURL
 		})
