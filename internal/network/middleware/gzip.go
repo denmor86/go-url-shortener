9c623748770f1ed2baca6d78b8a781b6d3f79ebf
Morozov Denis <denmor86@gmail.com>
2025-04-17 22:54:35 +0300

    Update comments

--- internal/network/middleware/gzip.go
+++ internal/network/middleware/gzip.go
@@ -26,7 +26,15 @@ func (c *compressWriter) Header() http.Header {
 }
 
 func (c *compressWriter) Write(p []byte) (int, error) {
-	return c.zw.Write(p)
+	contentType := c.w.Header().Get("Content-Type")
+	isJsonContent := strings.Contains(contentType, "application/json")
+	isHTMLContent := strings.Contains(contentType, "text/html")
+	isPlainContent := strings.Contains(contentType, "text/plain")
+	// упаковываем контент определенных типов
+	if isJsonContent || isHTMLContent || isPlainContent {
+		return c.zw.Write(p)
+	}
+	return c.w.Write(p)
 }
 
 func (c *compressWriter) WriteHeader(statusCode int) {
@@ -71,40 +79,32 @@ func (c *compressReader) Close() error {
 	return c.zr.Close()
 }
 
+// GzipHandle — middleware-gzip для HTTP-запросов.
 func GzipHandle(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// по умолчанию устанавливаем оригинальный http.ResponseWriter как тот,
-		// который будем передавать следующей функции
+
 		ow := w
 
-		// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
 		acceptEncoding := r.Header.Get("Accept-Encoding")
 		supportsGzip := strings.Contains(acceptEncoding, "gzip")
 		if supportsGzip {
-			// оборачиваем оригинальный http.ResponseWriter новым с поддержкой сжатия
 			cw := newCompressWriter(w)
-			// меняем оригинальный http.ResponseWriter на новый
 			ow = cw
-			// не забываем отправить клиенту все сжатые данные после завершения middleware
 			defer cw.Close()
 		}
 
-		// проверяем, что клиент отправил серверу сжатые данные в формате gzip
 		contentEncoding := r.Header.Get("Content-Encoding")
 		sendsGzip := strings.Contains(contentEncoding, "gzip")
 		if sendsGzip {
-			// оборачиваем тело запроса в io.Reader с поддержкой декомпрессии
 			cr, err := newCompressReader(r.Body)
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
-			// меняем тело запроса на новое
 			r.Body = cr
 			defer cr.Close()
 		}
 
-		// передаём управление хендлеру
 		h.ServeHTTP(ow, r)
 	}
 }
